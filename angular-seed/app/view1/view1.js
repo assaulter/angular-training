34b47933e38c062e88481541346226100566662a
KazukiKubo <kazukikubo@KazukiKubo-no-MacBook-Pro.local>
2015-09-04 13:33:09 +0900

    簡単なアイテムの追加と表示を実装

--- angular-seed/app/view1/view1.js
+++ angular-seed/app/view1/view1.js
@@ -5,10 +5,22 @@ angular.module('myApp.view1', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
+    controller: 'View1Ctrl',
+    controllerAs: 'View1Ctrl'
   });
 }])
 
 .controller('View1Ctrl', [function() {
+  var controller = this;
+  controller.todos = [
+    {title: 'タスク１', done: false},
+    {title: 'タスク２', done: true},
+  ];
 
+  controller.addTodo = function(newTodo) {
+    console.log(newTodo);
+    controller.todos.push({
+      title: newTodo.title, done: false
+    });
+  };
 }]);
